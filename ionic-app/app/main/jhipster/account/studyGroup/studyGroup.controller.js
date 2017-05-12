a0a7e7d81b3b553c1b2e16c609c334b2a13aaff1
Valerio <valeriosofi@gmail.com>
2017-05-12 14:27:18 +0200

    funziona cambia gruppo

--- ionic-app/app/main/jhipster/account/studyGroup/studyGroup.controller.js
+++ ionic-app/app/main/jhipster/account/studyGroup/studyGroup.controller.js
@@ -5,10 +5,42 @@
         .module('main')
         .controller('studyGroupController', studyGroupController);
 
-    studyGroupController.$inject = [];
+    studyGroupController.$inject = ['$window', '$timeout', 'studyGroupService', 'addStudentService'];
 
-    function studyGroupController () {
+    function studyGroupController ($window, $timeout, studyGroupService, addStudentService) {
+        var vm = this;
+        vm.salvato = true;
+        vm.avviso = false;
+        vm.student = [];
         
+        vm.save = save;
+        vm.studygroups = studyGroupService.query();
+        
+        loadAll();
+        
+        function loadAll() {
+            addStudentService.query(function(result) {
+                vm.student = result[0];
+            });
+        }
+        function save () {
+            vm.isSaving = true;
+            console.log(vm.student);
+            addStudentService.update(vm.student, onSaveSuccess, onSaveError);
+        }
 
+        function onSaveSuccess (result) {
+            console.log("Gruppo creato!");
+            vm.salvato = false;
+            vm.avviso = true;
+            $timeout(function () {
+                $window.location.reload();
+                vm.avviso = false;
+            }, 1500);
+        }
+
+        function onSaveError () {
+            vm.isSaving = false;
+        }
     }
 })();
